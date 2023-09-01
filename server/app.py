12f2462f6510d9c87cfa2dfe57983adc57822127
Kevin Bravo <kbravo1024@gmail.com>
2023-09-01 00:43:20 -0700

    complete flask sqlalchemy lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,39 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response_body = f'''
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper}</ul>
+        <ul>Enclosure: {animal.enclosure}</ul>
+    '''
+    return response_body
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+        <ul>Animal: {zookeeper.animals}</ul>
+    '''
+    return response_body
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+        <ul>Animal: {enclosure.animals}</ul>
+    '''
+    return response_body
 
 
 if __name__ == '__main__':
